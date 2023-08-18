42ebd1fbfb02e0e420ac24fb2160b4498e486b5a
Moaz-ashraf1 <moaza2298@gmail.com>
2023-08-18 07:39:06 +0300

    Improved stripe implementation

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,7 +7,7 @@ export const bookTour = async tourId => {
     );
 
     // Get checkout session from API
-    const session = await axios(`/api/v1/booking/checkout-session/${tourId}`);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id
     });
