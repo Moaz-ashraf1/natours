fb481fe80bdbf309840466b38aadf2285c08707e
Moaz-ashraf1 <moaza2298@gmail.com>
2023-08-18 09:12:41 +0300

    Implemented stripe message

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -3,14 +3,7 @@ const AppError = require('../utils/appError');
 const Tour = require('./../models/tourModel');
 const User = require('./../models/userModel');
 const Booking = require('./../models/bookingModel');
-exports.alerts = (req, res, next) => {
-  const { alert } = req.query;
-  if (alert === 'booking') {
-    res.locals.alert =
-      "Your booking was successful! please check your email for a confirmation. If your booking doesn't show up here immediatly, please come back later.";
-    next();
-  }
-};
+
 exports.getOverview = async (req, res) => {
   const tours = await Tour.find();
   res.status(200).render('overview', {
--- public/js/index.js
+++ public/js/index.js
@@ -69,6 +69,3 @@ if (bookBtn) {
     await bookTour(tourId);
   });
 }
-
-const alertMessage = document.querySelector('body').dataset.alert;
-if (alertMessage) showAlert('success', alertMessage, 20);
